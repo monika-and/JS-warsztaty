a67bd4ddf4912d6c36c24376810a55f6a17827a6
monika-and <monica.andrzejewska@gmail.com>
2017-06-11 14:25:17 +0200

    json

---
+++ js/json.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var jsonPracownicy = {  
+ "pracownicy":[
+    {"firstName": "Krystian", "lastName": "Dziopa"},
+    {"firstName": "Anna", "lastName": "Szapiel"},
+    {"firstName": "Piotr", "lastName": "Żmuda"}
+ ]
+};
+
+console.log(jsonPracownicy);
+
+jsonPracownicy.pracownicy.forEach(function(el, i){
+            console.log("Imię i nazwisko " + (i+1) + ": " + el.firstName + el.lastName);
+}) 
