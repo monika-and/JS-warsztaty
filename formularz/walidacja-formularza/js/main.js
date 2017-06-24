9f55ad57310f71228d4ee4fb6e1475e6432b72c4
monika-and <monica.andrzejewska@gmail.com>
2017-06-24 13:34:11 +0200

    walidacja

--- formularz/walidacja-formularza/js/main.js
+++ formularz/walidacja-formularza/js/main.js
@@ -1,21 +1,123 @@
 'use strict';
 
-var imie = document.getElementById('name');
+var wszystkieZgody = document.getElementById("wszystkie-zgody")//.addEventListener("change", function() {
+ //   console.log('klikniety');
+    //}
+//);
+var zgodaMarketingowa1 = document.getElementById("zgoda-marketingowa-1");
+var zgodaMarketingowa2 = document.getElementById("zgoda-marketingowa-2");
+var wyslijBtn = document.getElementById("wyslij");
+var inputImie = document.getElementById("name");
+var inputEmail = document.getElementById("email");
+var wiadomosc = document.getElementById("wiadomosc");
 
-var mail = document.getElementById('email');
-
-var zgoda1 = document.getElementById('zgoda-marketingowa-1');
-
-
-if (imie !== null && imie.value === "") {
-    console.log('brak imienia i nazwiska'); 
-} else if (mail !== null && imie.value === ""){
-    console.log('brak maila')
+function stanCheckboxa() {
+    zgodaMarketingowa1.checked = this.checked;
+    zgodaMarketingowa2.checked = this.checked;
+    
+    zgodaMarketingowa1.disabled = this.checked;
+    zgodaMarketingowa2.disabled = this.checked;
 }
 
 
-checkbox.checked 
+wszystkieZgody.onchange = stanCheckboxa;
 
+
+function walidujFormularz(event) {
+    
+
+   wiadomosc.innerHTML = '<ul id="wiadomosc"></ul>'
+    
+    if(inputImie.value.trim() == "") {
+        
+        var msgImie = document.createElement("li");
+        msgImie.innerHTML = "Wpisz Imię!";
+        wiadomosc.appendChild(msgImie);
+       
+        event.preventDefault();
+    } 
+    
+    
+     if(inputEmail.value.trim() == "") {
+      
+         var msgEmail = document.createElement("li");
+         msgEmail.innerHTML = "Wpisz Email!";
+         wiadomosc.appendChild(msgEmail);
+         
+        event.preventDefault();
+    } 
+    
+    
+    if(! zgodaMarketingowa1.checked) {
+        
+        var msgZgodaMarketingowa = document.createElement("li");
+         msgZgodaMarketingowa.innerHTML = "musisz wyrazić zgodę marketingową 1!";
+         wiadomosc.appendChild(msgZgodaMarketingowa);
+        
+        event.preventDefault();
+    }
+    
+}
+
+
+wyslijBtn.addEventListener('click', walidujFormularz);
+
+
+/* var wszystkieCheckboxy = document.querySelectorAll("input[type=checkbox]");
+
+
+wszystkieZgody.onchange = function() {
+    console.log("klikniety");
+    console.log(this.checked);
+}
+
+//if (wszystkieZgody.checked) {
+//    console.log(zgodaMarketingowa1.checked);
+//}
+
+
+console.log(wszystkieZgody);
+console.log(wszystkieCheckboxy);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+//checkbox.checked
+
+
+//document.addEventListener("click", function()) {
+//                          console.log("klikniety");
+//                          }
+
+
+
+//var imie = document.getElementById('name');
+//
+//var mail = document.getElementById('email');
+//
+//var zgoda1 = document.getElementById('zgoda-marketingowa-1');
+//
+//
+//if (imie != null && imie.value == "") {
+//    console.log('brak imienia i nazwiska'); 
+//} 
+
+//if (mail != null && imie.value === ""){
+//    console.log('brak maila')
+//}
+//
+//
+//checkbox.checked 
+//
 
 
 
