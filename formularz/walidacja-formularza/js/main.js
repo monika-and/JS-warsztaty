d5f9d6bbd310162d6383bf05240523774407a7ad
monika-and <monica.andrzejewska@gmail.com>
2017-06-24 10:48:30 +0200

    init

---
+++ formularz/walidacja-formularza/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var imie = document.getElementById('name');
+
+var mail = document.getElementById('email');
+
+var zgoda1 = document.getElementById('zgoda-marketingowa-1');
+
+
+if (imie !== null && imie.value === "") {
+    console.log('brak imienia i nazwiska'); 
+} else if (mail !== null && imie.value === ""){
+    console.log('brak maila')
+}
+
+
+checkbox.checked 
+
+
+
+
+
+
+
+
+/*buttony - onclick zdarzenie - clasa (onclick +metoda, który klawisz klikniety i ma sie wpisywac do imputa)
+
+funkcje matematyczne
+string ktory mozna zrobic przez funkcje ewaluate eval
+bez obslugi błędu
+valuate
+funkcja w momencie klikniecia = ma pobrac tekst z inputa i wrzucic do funkcji eval, i zwrocic tekst jako wynik
+*/
